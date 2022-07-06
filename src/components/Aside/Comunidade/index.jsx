c6b656fe2508506625b199ee3b942347a6fdb16e
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-06 16:40:05 -0300

    feat: Lista da Comunidade transformada em componente.

---
+++ src/components/Aside/Comunidade/index.jsx
@@ -0,0 +1,36 @@
+const Comunidade = () => {
+
+    return (
+        <div>
+            <span>COMUNIDADE</span>
+            
+            <ul>
+                <li>
+                    <a href="#">Fórum</a>
+                </li>
+                <li>
+                    <a href="#">Academy</a>
+                </li>
+                <li>
+                    <details>
+                        <summary>
+                            Eventos
+                        </summary>
+                        <a href="#">Evento 1</a>
+                        <a href="#">Evento 2</a>
+                        <a href="#">Evento 3</a>
+                    </details>
+                </li>
+                <li>
+                    <a href="#">Notícias</a>
+                </li>
+                <li>
+                    <a href="#">Serviços</a>
+                </li>
+            </ul>
+
+        </div>
+    )
+}
+
+export default Comunidade;
