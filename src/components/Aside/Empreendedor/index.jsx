69c4ed3c93eb15c1bcccf98c26cfda438a71e682
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-06 16:40:40 -0300

    feat: Lista da Área do Empreendedor transformada em componente.

---
+++ src/components/Aside/Empreendedor/index.jsx
@@ -0,0 +1,40 @@
+const Empreendedor = () => {
+
+    return (
+        <div>
+            <span>ÁREA DO EMPREGADOR</span>
+
+            <ul>
+                <li>
+                    <a href="#">Dashboard</a>
+                </li>
+                <li>
+                    <a href="#">Empresas</a>
+                </li>
+                <li>
+                    <details>
+                        <summary>
+                            Projetos
+                        </summary>
+                        <a href="#">Projeto 1</a>
+                        <a href="#">Projeto 2</a>
+                        <a href="#">Projeto 3</a>
+                    </details>
+                </li>
+                <li>
+                    <a href="#">Chat</a>
+                </li>
+                <li>
+                    <a href="#">Proventos</a>
+                </li>
+                <li>
+                    <a href="#">Indicações</a>
+                </li>
+            </ul>
+
+        </div>
+
+    )
+}
+
+export default Empreendedor;
