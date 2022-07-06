e454623eb9d5cf31c100474f063ea4ac3f5767c9
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-06 19:45:26 -0300

    feat: Clicando na logo voltará para a página inicial.

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -3,14 +3,17 @@ import bloxsLogo from "../../assets/bloxsLogo.svg";
 
 import Comunidade from "./Comunidade";
 import Empreendedor from "./Empreendedor";
+import { useHistory } from "react-router-dom";
 
 const Aside = () => {
+
+    const history = useHistory()
 
     return (
         <aside className="mainContent__aside">
 
             <figure className="aside__image">
-                <img src={bloxsLogo} alt="Logo da Bloxs" />
+                <img src={bloxsLogo} alt="Logo da Bloxs" onClick={() => history.push("/")}/>
                 <figcaption>Logo da Bloxs</figcaption>
             </figure>
 
