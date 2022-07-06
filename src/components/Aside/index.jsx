97ad471293939a6d65e6ca88312389bbdfca7623
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-06 18:53:19 -0300

    feat: Atribuindo classes para a estilização.

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,20 +1,22 @@
+import "./style.scss"
 import bloxsLogo from "../../assets/bloxsLogo.svg";
+
 import Comunidade from "./Comunidade";
 import Empreendedor from "./Empreendedor";
 
 const Aside = () => {
 
     return (
-        <aside>
+        <aside className="mainContent__aside">
             <img src={bloxsLogo} alt="Logo da Bloxs" />
 
-            <nav>
+            <nav className="aside__nav">
                 <a href="#">Home</a>
                 <Empreendedor />
                 <Comunidade />
             </nav>
 
-            <button>Submeter projeto</button>
+            <button className="aside__button">Submeter projeto</button>
 
         </aside>
     )
