d2d5fb8036f27132f37136d1c0578586ab6d06d2
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-06 16:41:57 -0300

    refactor: Mudança para a utilização de componentes.

--- src/components/Aside/index.jsx
+++ src/components/Aside/index.jsx
@@ -1,4 +1,6 @@
 import bloxsLogo from "../../assets/bloxsLogo.svg";
+import Comunidade from "./Comunidade";
+import Empreendedor from "./Empreendedor";
 
 const Aside = () => {
 
@@ -8,73 +10,10 @@ const Aside = () => {
 
             <nav>
                 <a href="#">Home</a>
-
-                <div>
-
-                    <span>ÁREA DO EMPREGADOR</span>
-
-                    <ul>
-                        <li>
-                            <a href="#">Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="#">Empresas</a>
-                        </li>
-                        <li>
-                            <details>
-                                <summary>
-                                    Projetos
-                                </summary>
-                                <a href="#">Projeto 1</a>
-                                <a href="#">Projeto 2</a>
-                                <a href="#">Projeto 3</a>
-                            </details>
-                        </li>
-                        <li>
-                            <a href="#">Chat</a>
-                        </li>
-                        <li>
-                            <a href="#">Proventos</a>
-                        </li>
-                        <li>
-                            <a href="#">Indicações</a>
-                        </li>
-                    </ul>
-
-                </div>
-
-                <div>
-
-                    <span>COMUNIDADE</span>
-
-                    <ul>
-                        <li>
-                            <a href="#">Fórum</a>
-                        </li>
-                        <li>
-                            <a href="#">Academy</a>
-                        </li>
-                        <li>
-                            <details>
-                                <summary>
-                                    Eventos
-                                </summary>
-                                <a href="#">Evento 1</a>
-                                <a href="#">Evento 2</a>
-                                <a href="#">Evento 3</a>
-                            </details>
-                        </li>
-                        <li>
-                            <a href="#">Notícias</a>
-                        </li>
-                        <li>
-                            <a href="#">Serviços</a>
-                        </li>
-                    </ul>
-
-                </div>
+                <Empreendedor />
+                <Comunidade />
             </nav>
-
+            
         </aside>
     )
 }
