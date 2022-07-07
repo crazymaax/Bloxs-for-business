455381b57cd51f88abf5588099d39b0494a3f21a
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-07 16:24:59 -0300

    feat: NewsProvider adicionado aos provedores.

--- src/providers/index.jsx
+++ src/providers/index.jsx
@@ -1,10 +1,13 @@
+import { NewsProvider } from "./noticias";
 import { UserProvider } from "./user";
 
 const Providers = ({ children }) => {
     return (
-        <UserProvider>
-            {children}
-        </UserProvider>
+        <NewsProvider>
+            <UserProvider>
+                {children}
+            </UserProvider>
+        </NewsProvider>
     )
 };
 export default Providers;
