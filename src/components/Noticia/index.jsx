574f5cb6288a9d4300fd8e3c50c6e5de19587eae
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-07 19:17:40 -0300

    feat: Transformando em âncora para enviar usuário para a página da notícia.

--- src/components/Noticia/index.jsx
+++ src/components/Noticia/index.jsx
@@ -6,18 +6,22 @@ const Noticia = ({ noticia }) => {
 
     const { FormatDate } = useNews()
 
-    const { modified, title, yoast_head_json: { og_image } } = noticia
+    const { modified, title, link, yoast_head_json: { og_image } } = noticia
 
     const photoUrl = og_image[0].url
     const itemTitle = title.rendered
     const dateFormatted = FormatDate(modified)
 
+    const handleNewClick= () => {
+        location.href = link
+    }
+
     return (
-        <div className="list__container">
+        <a className="list__container" href={link} target="_blank">
             <img src={photoUrl} alt={itemTitle} />
             <strong>{itemTitle}</strong>
             <span>{dateFormatted}</span>
-        </div>
+        </a>
 
     )
 }
