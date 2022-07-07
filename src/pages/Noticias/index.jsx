b56fc6ad1a977007917983dadefbb28a77de5794
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-07 11:34:13 -0300

    feat: Verificação de autenticidade.

--- src/pages/Noticias/index.jsx
+++ src/pages/Noticias/index.jsx
@@ -1,7 +1,17 @@
 import Aside from "../../components/Aside/index"
 import Header from "../../components/Header"
 
+import { useAuth } from "../../providers/user"
+import { useHistory } from "react-router-dom"
+
 const Noticias = () => {
+
+    const { user } = useAuth()
+    const history = useHistory()
+
+    if (!user) {
+        history.push("/")
+    }
 
     return (
         <div className="pageContent">
