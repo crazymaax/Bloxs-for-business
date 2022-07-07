7912eb6811233f6c66e10374bbf4d14465e0ac21
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-06 22:17:53 -0300

    feat: Aplicando a rota de login no Switch.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,13 @@
 import { Route, Switch } from "react-router-dom";
 
+import Login from "../pages/Login";
 import Noticias from "../pages/Noticias"
 
 const Routes = () => {
 
     return (
         <Switch>
+            <Route exact path="/" component={Login} />
             <Route exact path="/noticias" component={Noticias} />
         </Switch>
     )
