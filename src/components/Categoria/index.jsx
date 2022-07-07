5362310e678a2e37105ce366e0eb1e991baf3735
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-07 12:15:21 -0300

    feat: Criação estática das notícias.

---
+++ src/components/Categoria/index.jsx
@@ -0,0 +1,52 @@
+import "./style.scss"
+
+const Categoria = () => {
+
+    return (
+        <div className="main__category">
+            <h2>Energia</h2>
+
+            <div className="category__list">
+                <div className="list__container">
+                    <img src="https://img.olhardigital.com.br/wp-content/uploads/2018/09/20180903133856-860x450.jpg" alt="" />
+                    <strong>titulo da noticia</strong>
+                    <span>data da noticia</span>
+                </div>
+                <div className="list__container">
+                    <img src="https://img.olhardigital.com.br/wp-content/uploads/2018/09/20180903133856-860x450.jpg" alt="" />
+                    <strong>titulo da noticia</strong>
+                    <span>data da noticia</span>
+                </div>
+                <div className="list__container">
+                    <img src="https://img.olhardigital.com.br/wp-content/uploads/2018/09/20180903133856-860x450.jpg" alt="" />
+                    <strong>titulo da noticia</strong>
+                    <span>data da noticia</span>
+                </div>
+            </div>
+
+            <h2>Agronegócio</h2>
+
+            <div className="category__list">
+                <div className="list__container">
+                    <img src="https://img.olhardigital.com.br/wp-content/uploads/2018/09/20180903133856-860x450.jpg" alt="" />
+                    <strong>titulo da noticia</strong>
+                    <span>data da noticia</span>
+                </div>
+                <div className="list__container">
+                    <img src="https://img.olhardigital.com.br/wp-content/uploads/2018/09/20180903133856-860x450.jpg" alt="" />
+                    <strong>titulo da noticia</strong>
+                    <span>data da noticia</span>
+                </div>
+                <div className="list__container">
+                    <img src="https://img.olhardigital.com.br/wp-content/uploads/2018/09/20180903133856-860x450.jpg" alt="" />
+                    <strong>titulo da noticia</strong>
+                    <span>data da noticia</span>
+                </div>
+            </div>
+
+        </div>
+
+    )
+}
+
+export default Categoria
