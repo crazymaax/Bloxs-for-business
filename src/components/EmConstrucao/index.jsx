5a99688ebf042a49e90c9902077af81645db8fc8
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-08 11:05:02 -0300

    feat: Criação da página: Em construção.

---
+++ src/components/EmConstrucao/index.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import "./style.scss"
+
+import UnderConstruction from "../../assets/icons/under-construction.svg"
+
+const EmConstrucao = ({ slug }) => {
+
+    const [pageName, setPageName] = useState()
+
+    useEffect(() => {
+        const slugFormater = () => {
+            const newSlug = slug.slice(1, slug.length).replaceAll("_", " ")
+            const newSlugFormatted = newSlug.charAt(0).toUpperCase() + newSlug.slice(1);
+    
+            setPageName(newSlugFormatted)
+        }
+
+        slugFormater()
+
+    } ,[])
+
+
+    return (
+        <div className="main__underConstructionContainer">
+            {slug && (
+                <h2>{pageName}</h2>
+            )}
+            <div className="underConstructionContainer__constructionDiv">
+                <img src={UnderConstruction} alt="Em construção"  className="constructionDiv__image"/>
+                <h3>Em construção</h3>
+                <p>Estamos em constante crescimento e em breve está página ficará disponível, por favor volte novamente em breve. </p>
+            </div>
+        </div>
+    )
+}
+
+export default EmConstrucao;
