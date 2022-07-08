97197807071e2c2885218c78ec875a61fc57c303
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-08 11:38:32 -0300

    feat: Criação da página fórum.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,12 +5,14 @@ import Academy from "../pages/Comunidade/Academy";
 import Eventos from "../pages/Comunidade/Eventos";
 import Noticias from "../pages/Comunidade/Noticias"
 import Servicos from "../pages/Comunidade/Servicos";
+import Forum from "../pages/Comunidade/Forum";
 
 const Routes = () => {
 
     return (
         <Switch>
             <Route exact path="/" component={Login} />
+            <Route exact path="/comunidade/forum" component={Forum} />
             <Route exact path="/comunidade/academy" component={Academy} />
             <Route exact path="/comunidade/eventos:slug" component={Eventos} />
             <Route exact path="/comunidade/noticias" component={Noticias} />
