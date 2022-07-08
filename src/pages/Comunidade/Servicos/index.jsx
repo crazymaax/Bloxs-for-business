4e3e67b6f16980756c2c9072d9e675fff4fda68d
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-08 11:31:15 -0300

    feat: Criação da página serviços.

---
+++ src/pages/Comunidade/Servicos/index.jsx
@@ -0,0 +1,25 @@
+import Aside from "../../../components/Aside";
+import EmConstrucao from "../../../components/EmConstrucao";
+import Header from "../../../components/Header";
+
+const Servicos = () => {
+
+    const slug = ":serviços" // Enquanto não possuir slug, foi definido um temporariamente.
+
+    return (
+        <>
+            <div className="pageContent">
+                <Aside />
+                <div className="pageContent__main">
+
+                    <Header PageName={"Serviços"} />
+                    <main>
+                        <EmConstrucao slug={slug}/>
+                    </main>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Servicos;
