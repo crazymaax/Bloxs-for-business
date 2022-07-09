0348c36f6f34a3d4c917879ebc437e63fc5893c1
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-09 12:53:59 -0300

    feat: Modal ao clicar em configurações de usuário no Header.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,32 @@
+import { useState, useEffect } from "react"
 import "./style.scss"
 
 import Bell from "../../assets/icons/bell.svg"
+
+import { FaUserAlt } from "react-icons/fa"
+import { ImExit } from "react-icons/im"
+import { BsFillGearFill } from "react-icons/bs"
+
 import { useAuth } from "../../providers/user"
-import { useHistory } from "react-router-dom"
 
 const Header = ({ PageName }) => {
 
-    const { user } = useAuth()
+    const { user, setUser } = useAuth()
     const { avatar, name } = user
+
+    const [isOpenModal, setIsOpenModal] = useState(false)
+
+    useEffect(() => {
+
+        document.addEventListener("click", (e) => {
+            if (!e.target.closest(".user__modalConfiguration")) {
+                setIsOpenModal(false)
+            } else {
+                setIsOpenModal(true)
+            }
+        })
+
+    }, [isOpenModal])
 
     return (
         <header className="main__header">
@@ -16,9 +35,26 @@ const Header = ({ PageName }) => {
                 <button className="user__notification">
                     <img src={Bell} alt="Ícone de notificações" />
                 </button>
+                <div className="user__modalConfiguration" >
+                    <FaUserAlt size={20} color={"#414368"} />
+
+                    {isOpenModal && (
+                        <div className="user__modal">
+                            <button>
+                                <BsFillGearFill color={"#414368"} />
+                                Configurações
+                            </button>
+                            <button onClick={() => setUser()}>
+                                <ImExit color={"#414368"} />
+                                Sair
+                            </button>
+                        </div>
+                    )}
+
+                </div>
 
                 <span>{name}</span>
-                <div className="user__image">
+                <div className="user__image" >
                     <img src={avatar} alt={name} />
                 </div>
 
