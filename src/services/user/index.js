885b8f010946839714b91fa30aee5e5166c507dc
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-12 10:36:34 -0300

    feat: Requisição na API para registrar usuários.

--- src/services/user/index.js
+++ src/services/user/index.js
@@ -1,5 +1,24 @@
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import { app } from "../../services"; //Necessário a importação para o Login do Google
+import { app, instanceUser } from "../../services"; //Necessário a importação para o Login do Google
+import { toast } from "react-toastify";
+
+export const signUpUser = async (data) => {
+    const response = await instanceUser
+        .post("/register", data)
+        .then((response) => {
+            return true;
+        })
+        .catch((error) => {
+            if (error.response.data === "Email already exists") {
+                toast.error("Email já cadastrado!");
+            } else {
+                toast.error("Algo deu errado!");
+            }
+        });
+
+    return response;
+};
+
 
 export const loginWithGoogle = () => {
     const provider = new GoogleAuthProvider();
