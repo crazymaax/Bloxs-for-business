9a90f7af0a679df2ebaaff4ac326dc99e13a5893
Max Milan Campos de Oliveira <95248203+crazymaax@users.noreply.github.com>
2022-07-12 10:48:54 -0300

    feat: Função de login de usuários com a api.

--- src/services/user/index.js
+++ src/services/user/index.js
@@ -2,6 +2,27 @@ import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { app, instanceUser } from "../../services"; //Necessário a importação para o Login do Google
 import { toast } from "react-toastify";
 
+export const loginUser = async (data) => {
+    const response = await instanceUser
+        .post("/login", data)
+        .then((response) => {
+            return response.data.user;
+        })
+        .catch((error) => {
+            if (error.response.data === "Incorrect password") {
+                toast.error("Senha incorreta!");
+            } else if (error.response.data === "Cannot find user") {
+                toast.error("Email não cadastrado!");
+            } else {
+                toast.error("Algo deu errado!");
+            }
+
+            return false;
+        });
+
+    return response;
+};
+
 export const signUpUser = async (data) => {
     const response = await instanceUser
         .post("/register", data)
